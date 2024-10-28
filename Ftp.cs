f99e3641fa8ba4052c4cf4c7134284f4ac75411a
Ali Hagh <alihagh1383@gmail.com>
2024-10-28 13:04:49 +0330

    Add project files.

---
+++ Ftp.cs
@@ -0,0 +1,196 @@
+﻿using System.Collections.Immutable;
+using System.Net;
+using System.Text;
+using FluentFTP;
+
+namespace MrX.Connections
+{
+    public class Ftp
+    {
+        private FtpClient _client;
+
+        private string _rootPath;
+        private string FilePath(string file) => (_rootPath + "/" + file);
+
+        public Ftp(string hostName, string? userName, string? password, string root)
+        {
+            var credentials = new NetworkCredential(userName, password);
+            _client = new FtpClient();
+            _client.Host = hostName;
+            _client.Credentials = credentials;
+            _client.Connect();
+            if (Exists(root, out bool ex) && !ex)
+                CreateDirectory(root);
+            _rootPath = root;
+        }
+
+        public bool Opened
+        {
+            get
+            {
+                if (_client.IsConnected) return _client.IsConnected;
+                else return Open();
+            }
+        }
+
+        public bool Open()
+        {
+            _client.Connect();
+            return _client.IsConnected;
+        }
+
+        public bool DownloadFile(string fileName, out Stream file)
+        {
+            file = new MemoryStream();
+            if (!Opened)
+            {
+                return false;
+            }
+
+            try
+            {
+                _client.DownloadStream(file, FilePath(fileName));
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool DownloadFile(string fileName, out string file)
+        {
+            if (!DownloadFile((fileName), out Stream fileStream))
+            {
+                file = "";
+                return false;
+            }
+
+            try
+            {
+                fileStream.Position = 0;
+                var streamReader = new StreamReader(fileStream);
+                file = streamReader.ReadToEnd();
+                return true;
+            }
+            catch
+            {
+                file = "";
+                return false;
+            }
+        }
+
+        public bool UpLoadFile(string fileName, Stream file, bool overwrite = true)
+        {
+            if (!Opened) return false;
+            try
+            {
+                _client.UploadStream(file, FilePath(fileName),
+                    (overwrite) ? FtpRemoteExists.Overwrite : FtpRemoteExists.Resume);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool UpLoadFile(string fileName, string fileText, bool overwrite = true)
+        {
+            var file = new MemoryStream(Encoding.UTF8.GetBytes(fileText));
+            return UpLoadFile(fileName, file, overwrite);
+        }
+
+        public bool CreateFile(string fileName, bool overwrite = true)
+        {
+            if (!Opened) return false;
+            try
+            {
+                _client.UploadStream(new MemoryStream(), FilePath(fileName),
+                    (overwrite) ? FtpRemoteExists.Overwrite : FtpRemoteExists.Resume);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Exists(string fileName, out bool exist)
+        {
+            if (!Opened)
+            {
+                {
+                    exist = false;
+                    return false;
+                }
+            }
+
+            try
+            {
+                exist = _client.FileExists(FilePath(fileName));
+                return true;
+            }
+            catch
+            {
+                exist = false;
+                return false;
+            }
+        }
+
+        public bool CreateDirectory(string directory)
+        {
+            if (!Opened) return false;
+            try
+            {
+                _client.CreateDirectory(FilePath(directory));
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(string fileName)
+        {
+            if (!Opened)
+            {
+                {
+                    return false;
+                }
+            }
+
+            try
+            {
+                _client.DeleteFile(FilePath(fileName));
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool ListOfFiles(string dir, out FtpListItem[] files)
+        {
+            if (!Opened)
+            {
+                files = new FtpListItem[0];
+                return false;
+            }
+
+            try
+            {
+                files = _client.GetListing(FilePath(dir));
+                
+                return true;
+            }
+            catch
+            {
+                files = new FtpListItem[0];
+                return false;
+            }
+        }
+    }
+}
---
+++ Local.cs
@@ -0,0 +1,172 @@
+﻿using FluentFTP;
+using FluentFTP.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MrX.Connections
+{
+    public class Local
+    {
+
+        private string _rootPath;
+        private string FilePath(string file) => Path.Combine(AppDomain.CurrentDomain.BaseDirectory, _rootPath, file);
+
+        public Local(string root)
+        {
+            Console.WriteLine(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, root));
+            if (!File.Exists(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, root)))
+            {
+                Directory.CreateDirectory(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, root));
+            }
+            _rootPath = root;
+        }
+
+
+
+        public bool DownloadFile(string fileName, out Stream file)
+        {
+            file = new MemoryStream();
+
+
+            try
+            {
+                file = File.Open(FilePath(fileName), FileMode.OpenOrCreate);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool DownloadFile(string fileName, out string file)
+        {
+            if (!DownloadFile((fileName), out Stream fileStream))
+            {
+                file = "";
+                return false;
+            }
+
+            try
+            {
+                fileStream.Position = 0;
+                var streamReader = new StreamReader(fileStream);
+                file = streamReader.ReadToEnd();
+                fileStream.Close();
+                return true;
+            }
+            catch
+            {
+                file = "";
+                return false;
+            }
+        }
+
+        public bool UpLoadFile(string fileName, Stream file, bool overwrite = true)
+        {
+            try
+            {
+                var f = File.Open(FilePath(fileName), (!overwrite) ? FileMode.OpenOrCreate : FileMode.Create);
+
+                var WRITER = new StreamWriter(f);
+                WRITER.Write(new StreamReader(file).ReadToEnd());
+                WRITER.Flush();
+                f.Flush();
+                f.Close();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool UpLoadFile(string fileName, string fileText, bool overwrite = true)
+        {
+            var file = new MemoryStream(Encoding.UTF8.GetBytes(fileText));
+            return UpLoadFile(fileName, file, overwrite);
+        }
+
+        public bool CreateFile(string fileName, bool overwrite = true)
+        {
+
+            try
+            {
+                File.Create(FilePath(fileName)).Close();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Exists(string fileName, out bool exist)
+        {
+
+            try
+            {
+
+                exist = File.Exists(FilePath(fileName));
+                return true;
+            }
+            catch
+            {
+                exist = false;
+                return false;
+            }
+        }
+
+        public bool CreateDirectory(string directory)
+        {
+
+            try
+            {
+                var d = Directory.CreateDirectory(FilePath(directory));
+                d.Create();
+                Console.WriteLine(d.FullName);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(string fileName)
+        {
+
+
+            try
+            {
+                File.Delete(FilePath(fileName));
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool ListOfFiles(string dir, out string[] files)
+        {
+
+
+            try
+            {
+                files = Directory.GetFiles(FilePath(dir), "*", SearchOption.AllDirectories);
+
+                return true;
+            }
+            catch
+            {
+                files = new string[0];
+                return false;
+            }
+        }
+    }
+}
